50ef0386f413c1f7cc8ed1f7d1b9c6232a8fb0dc
Matthew Sigit <message4mas@gmail.com>
2024-03-27 02:26:47 -0400

    fixed issue w export

--- webapp/pages/api/search.ts
+++ webapp/pages/api/search.ts
@@ -1,1 +1,6 @@
-//currently using locally hosted ML model
+import type { NextApiRequest, NextApiResponse } from 'next';
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    //currently using locally hosted ML model
+    return;
+}
+export{}
