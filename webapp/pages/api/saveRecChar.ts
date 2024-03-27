d163d7e2ec20cfecd0eee042fe6c211a677ee9eb
nicksong04 <39754706+nicksong04@users.noreply.github.com>
2024-03-27 02:59:42 -0400

    remove error

--- webapp/pages/api/saveRecChar.ts
+++ webapp/pages/api/saveRecChar.ts
@@ -1,7 +1,7 @@
 // pages/api/history/save.js
 import clientPromise from '../../lib/mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
-import search from '../search';
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
@@ -21,12 +21,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         await db.collection("user_stats").updateOne(
             {"uuid" : uuid}, { $set: {"recChar": searchHistory[0]}, $inc: {"numSearches": 1}}, {upsert:true});
         
-        const name = "searchArray." + searchHistory[0];
-        var name2 = {smth:1};
-        for (var key in name2) {
-            key = name;
-        }
-
+        var name = "searchArray." + searchHistory[0];
+        var name2 = {} as { [key: string]: any };
+        name2[name] = 1;
         await db.collection("user_stats").updateOne(
             {"uuid" : uuid}, {$inc: name2}, {upsert:true});
         res.status(201).json({ message: 'History saved' });
