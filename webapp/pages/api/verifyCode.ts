649a4a57c64c2d399926f42cdb8252fd5ea984b3
Matthew Sigit <message4mas@gmail.com>
2024-03-29 04:01:00 -0400

    made 6dig auth

---
+++ webapp/pages/api/verifyCode.ts
@@ -0,0 +1,27 @@
+// pages/api/verifyCode.js
+import { NextApiRequest, NextApiResponse } from 'next';
+import clientPromise from '../../lib/mongodb';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { uuid, code } = req.query;
+
+    try {
+        const client = await clientPromise;
+        const db = client.db("account_info");
+
+        const user = await db.collection("user_info").findOne({ _id: uuid });
+        if (user == null) {
+            res.status(200).json({ success: false });
+        }
+        else {
+            if (user && user['6dig'] === code) {
+                res.status(200).json({ success: true });
+            } else {
+                res.status(200).json({ success: false });
+            }
+        }
+    } catch (error) {
+        console.error('Failed to verify code:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
--- webapp/pages/pending/index.tsx
+++ webapp/pages/pending/index.tsx
@@ -1,30 +1,54 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useAuth } from '../../context/AuthContext';
-import {app} from '../api/register';
 
-const pending = () => {
-    const resend = async() => {
-        const { email } = useAuth();
-        await app.emailPasswordAuth.resendConfirmation({ email });
-        return;
-    }
+const Pending = () => {
+    const { UUID, logIn } = useAuth();
+    const [code, setCode] = useState('');
+    const [error, setError] = useState('');
+    const router = useRouter();
+
+    const verifyCode = async () => {
+        if (!UUID) {
+            setError("There's an issue with your session. Please try to log in again.");
+            return;
+        }
+        try {
+            const response = await fetch(`/api/verifyCode?uuid=${UUID}&code=${code}`);
+            if (response.ok) {
+                const { success } = await response.json();
+                if (success) {
+                    logIn(UUID);
+                    router.push('/profile');
+                } else {
+                    setError('The code you entered is incorrect. Please try again.');
+                }
+            } else {
+                throw new Error('Failed to verify the code');
+            }
+        } catch (error) {
+            console.error('Error verifying code:', error);
+            setError('An error occurred while verifying your code. Please try again.');
+        }
+    };
 
     return (
-        <div className="min-h-screen bg-pl-1 dark:bg-pd-4">
+        <div className="min-h-screen bg-pl-1 dark:bg-pd-4 flex flex-col items-center justify-center">
             <h1 className="text-xl font-semibold">Authenticate Your Email</h1>
-            <p>Please check your email inbox for a authentication link.</p>
-            <p>If you didn't receive an email, please check your spam folder or, if you haven't registered, <Link legacyBehavior href="/register"><a className="text-white-100">try registering again</a></Link>.</p>
-            <button onClick={() => resend} className="py-2 px-4 rounded
-                        text-pl-3 border-2 border-rounded border-pl-3
-                        bg-pl-2
-                        dark:text-pd-3 dark:border-2 dark:border-rounded dark:border-pd-3
-                        dark:bg-pd-2">
-                        Resend Email
-                    </button>
+            <p>Please enter the 6-digit code sent to your email.</p>
+            {error && <p className="text-red-500">{error}</p>}
+            <input
+                type="text"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+                className="text-input"
+                placeholder="6-digit code"
+            />
+            <button onClick={verifyCode} className="btn">Verify Code</button>
+            <p>If you didn't receive an email, <Link href="/register"><a className="text-blue-500">try registering again</a></Link>.</p>
         </div>
     );
 };
 
-export default pending;
+export default Pending;
