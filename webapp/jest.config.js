f5e566208dfa069c9089566b71e4c117d565d7ab
Ethan <ethanling123@gmail.com>
2024-04-18 02:19:13 -0400

    jest config to js

---
+++ webapp/jest.config.js
@@ -0,0 +1,32 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+/** @type {import('jest').Config} */
+const config = {
+ // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  // collectCoverageFrom: undefined,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "babel",
+
+
+  // The test environment that will be used for testing
+  testEnvironment: "jsdom",
+};
+
+module.exports = createJestConfig(config);
