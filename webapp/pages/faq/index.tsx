5f441c5cf488067ff57bcceefd1478a5e2c3f9d9
Matthew Sigit <message4mas@gmail.com>
2024-04-18 23:20:45 -0400

    faq updates

--- webapp/pages/faq/index.tsx
+++ webapp/pages/faq/index.tsx
@@ -3,6 +3,8 @@ import ImageUploader from "../../components/ImageUploader";
 import { useAuth } from "../../context/AuthContext";
 import InfoTag from "../../components/Infotag";
 import * as blob_storage from "../../blob_storage";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const Faq = () => {
     const [activeIndex, setActiveIndex] = useState<number | null>(null);
@@ -10,7 +12,7 @@ const Faq = () => {
     const questionsAnswers = [
         {
             question: "How does the character identification work?",
-            answer: "Our platform uses a machine learning model based on Convolutional Neural Networks to accurately identify animated characters from user-uploaded images. More specifically, we trained a CNN using the triplet-loss framework, in which we choose two similar images and one different image and train the model with this scenario millions of times. Images were sourced by repositories of animated characters and/or labelled by us."
+            answer: "Our platform uses a machine learning model based on Convolutional Neural Networks to accurately identify animated characters from user-uploaded images. More specifically, we trained a CNN using the triplet-loss framework, in which we choose two similar images and one different image and train the model with this scenario millions of times. Images were sourced by repositories of animated characters and/or labelled by us. For the more technical users, please click the 'Technical Details: Why Triplet Loss?' question."
         },
         {
             question: "Can I use the platform for free?",
@@ -27,6 +29,10 @@ const Faq = () => {
         {
             question: "What data do you collect from users?",
             answer: "We only collect what users allow us to collect, and account deletion results in deletion and/or anonymization of all user data."
+        },
+        {
+            question: "Technical Details: Why Triplet Loss?",
+            answer: "Triplet loss is an effective training method for machine learning models, especially in distinguishing individuals in tasks like face recognition. It works by comparing three images: an anchor (a baseline image of one character), a positive (another image of the same character), and a negative (an image of a different character). This approach helps the model fine-tune features that differentiate similar characters, crucial in recognizing animated characters with similar traits. By minimizing the distance between the anchor and positive images while maximizing the distance from the negative, triplet loss creates a structured embedding space where similar features cluster and dissimilar ones are distanced. This enhances the model's ability to handle variations within the same character and ensures reliable identification in varied real-world conditions."
         },
     ];
 
@@ -100,37 +106,46 @@ const Faq = () => {
                             </div>
                         </div>
                     ))}
-                    <form onSubmit={submitFeedback} className="border rounded px-4 py-4 mt-4 dark:bg-pd-2">
+                    <div className="border rounded px-4 py-4 mt-4 dark:bg-pd-2">
+                    <form onSubmit={submitFeedback} >
                                         <label
                                             htmlFor="feedback"
                                             className="block mb-2 text-pl-3 dark:text-white">
-                                            Alternatively, Give Feedback Here:
+                                            Don't see your question? Ask us here!
                                         </label>
                                         <textarea
                                             id="feedback"
                                             value={feedback}
                                             onChange={handleFeedbackChange}
                                             className="px-4 border rounded p-2 w-full text-pl-3 dark:text-white dark:border-2 dark:border-rounded dark:border-pd-3 dark:bg-pd-4"
-                                            title="Write your feedback here"
+                                            title="Write your question here"
                                             rows={4}
-                                            placeholder="Hi! Please write any feedback here..."></textarea>
+                                            placeholder="Question..."></textarea>
                                         <button
                                             type="submit"
                                             className="animated-button px-2"
-                                            title="Submit Feedback"
+                                            title="Submit Question"
                                             disabled={submittingFeedback}>
                                             {submittingFeedback
                                                 ? "Submitting..."
-                                                : "Submit Feedback"}
+                                                : "Submit Question"}
                                         </button>
                                     </form>
                                     {revealThank && (
                                         <>
                                             <h2 className="dark:text-white font-semibold mb-4">
-                                                Thank you for submitting feedback / reaching out!
+                                                Thank you for reaching out!
                                             </h2>
                                         </>
                                     )}
+                        <label className="py-4 block mb-2 text-pl-3 dark:text-white">
+                            Or, Contact Us Directly:
+                        </label>
+                        <Link href="/contact" legacyBehavior>
+                                    <a className="animated-button"
+                                    title="Click to view contact information">Contact Us!</a>
+                        </Link>
+                        </div>
                 </div>
             </div>
         </div>
